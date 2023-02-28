3dbce3f5cfcc21aea2cb82f57dfef9b7a740e11c
kir0513 <kir0513@yandex.ru>
2023-02-28 11:44:46 +0300

    Переделал касс User, UserServiceImpl.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+        //создание, очистка и удаление таблицы пользователей
+        userDaoHibernate.createUsersTable();
+        userDaoHibernate.dropUsersTable();
+        userDaoHibernate.cleanUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.xdevapi.SessionFactory;
+import jm.task.core.jdbc.model.User;
+
+import java.lang.module.Configuration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+
 }
